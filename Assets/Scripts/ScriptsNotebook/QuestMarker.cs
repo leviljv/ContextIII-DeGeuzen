88441855c1ff9451ded37862963f7774a2b26ec5
Andries <61500390+Emerald001@users.noreply.github.com>
2023-01-26 11:10:31 +0100

    Every state can go into interaction state

--- Assets/Scripts/ScriptsNotebook/QuestMarker.cs
+++ Assets/Scripts/ScriptsNotebook/QuestMarker.cs
@@ -5,12 +5,10 @@ using UnityEngine.UI;
 
 public class QuestMarker : MonoBehaviour
 {
-
     public Sprite icon;
     [HideInInspector]public Image image;
 
-    public Vector2 position
-    {
-        get{ return new Vector2(transform.position.x, transform.position.z); }
+    public Vector2 position {
+        get { return new Vector2(transform.position.x, transform.position.z); }
     }
 }
